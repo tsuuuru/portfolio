9c969b3b52229633b617038c1c7258aba56e555c
tsu-ru <94231841+tsuuuru@users.noreply.github.com>
2023-12-03 12:00:27 +0900

    Delete blog directory

--- blog/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-document.getElementById('search-form').onsubmit = function(event) {
-    event.preventDefault();
-    const search = document.getElementById('search-form').word.value;
-    document.getElementById('output').textContent = `「${search}」の検索中...`;
-};
